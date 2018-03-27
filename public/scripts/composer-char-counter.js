20318433265230593d3f75f442621b99b34e34b0
saunderst <thomas130@gmx.com>
2018-03-27 19:58:23 -0400

    Implement the character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  console.log("char counter");
+  $('.new-tweet').find('textarea').on('keyup', function(ev) {
+    let $counter = $(this).closest('.new-tweet').find('.counter');
+    $counter.text(140 - $(this).val().length);
+    if ($(this).val().length > 140) {
+      // $counter.addClass('neg');
+      $counter.css({"color":"red"});
+    } else {
+      // $counter.removeClass('neg');
+      $counter.css({"color":""});
+    }
+  });
 });
