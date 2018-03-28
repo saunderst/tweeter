90fd424e14635a0d8f50df2bbc862b43ec1cf994
saunderst <thomas130@gmx.com>
2018-03-28 17:26:48 -0400

    Form Submission using JQuery

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,7 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
 function createTweetElement(tweet) {
   let $tweet = $('<article>').addClass('tweet');
   let timeAgo = ($.now() - tweet.created_at) / 1000 / 60 // minutes ago
@@ -37,58 +36,30 @@ function createTweetElement(tweet) {
   return $tweet;
 }
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
 function renderTweets(tweets) {
-  tweets.forEach((tweet) => $('#tweets').append(createTweetElement(tweet)));
+  tweets.forEach((tweet) => $('#tweets').prepend(createTweetElement(tweet)));
+}
+
+function showTweets() {
+  $.get("/tweets", data => {
+    renderTweets(data);
+  });
 }
 
 $(document).ready(function () {
-  renderTweets(data);
+  $.get("/tweets", data => {
+    renderTweets(data);
+  });
+  $(".new-tweet form").on("submit", function (event) {
+    event.preventDefault();
+    if ($(".new-tweet textarea").val().length > 140) {
+      alert('Gotta shorten up that tweet.');
+    } else {
+      $.post("/tweets", $(this).serialize(), () => {
+        $.get("/tweets", data => {
+          $('#tweets').prepend(createTweetElement(data[data.length - 1]));
+        });
+      });
+    }
+  });
 });
